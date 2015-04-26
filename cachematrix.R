49ae486a277bec95a721ef602cbfc9e8987f4320
Leptokurtic <warr@protonmail.ch>
2015-04-26 18:25:58 -0500

    Submission
    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## gets my assignment in on time 
+makeCacheMatrix <- function(blue.matrix = matrix()) {
+  # sets x as an empty matrix
+  red.matrix <- NULL
+  # Set the inverse equal to NULL
+  
+  if (!is.matrix(blue.matrix)) {
+    stop("I'm sorry. I can't do that, Dave")
+  }
+  #mocks user if supplied with non matrix object
+  set <- function(y){
+    blue.matrix <<- y
+    # asign x argument
+    red.matrix <<- NULL
+    # reset to null iteration control
+  }
+  get <- function() blue.matrix
+  # return matrix
+  
+  red.shift <- function(solve) red.matrix <<- solve
+  # invert
+  
+  get.shift <- function() red.matrix
+  # get.shift returns the Inverse
+  
+  list(set = set, get = get, red.shift= red.shift, get.shift= get.shift)
+  # creates a list of the functions
+  
 }
 
+# 2: cacheSolve: 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(blue.matrix, ...) {
+  
+  red.matrix <-  get.shift(blue.matrix)
+  # Retrives the most recent value for the inverse
+  
+  if(!is.null(red.matrix)){
+    message("Take the blue pill")
+    return(red.matrix)
+    # if value is anything but null- return previous matrix      
+  }
+  
+  message("Ingesting the red pill")
+  red.shift.request <-  get(blue.matrix)
+  red.matrix <- solve(red.shift.request)
+  blue.matrix$red.shift(red.matrix)
+  
+  red.matrix 
+  message("Remember to tip your program")
+  #fleecing user for money
 }
